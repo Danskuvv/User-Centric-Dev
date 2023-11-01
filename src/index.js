0e0770a1de91a0731975692be8eb39990e725b4a
Danskuvv <danielvv@metropolia.fi>
2023-11-01 11:32:24 +0200

    Express

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
-import http from "http";
-
+import express from "express";
 const hostname = "127.0.0.1";
+import path from "path";
+import { fileURLToPath } from "url";
+import { getItems } from "./items.js";
+import { getItemsById } from "./items.js";
+const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
-  //Content-Type header to JSON for all responses
-  res.setHeader("Content-Type", "application/json");
-
-  //JSON object esimerkki
-  const data = { message: "Welcome to my REST API!" };
-
-  //Handle different HTTP methods & endpoints.
-  if (req.method === "GET" && req.url === "/data") {
-    //read data from the server
-    res.writeHead(200);
-    res.end(JSON.stringify(data));
-  } else if (req.method === "POST" && req.url === "/data") {
-    //post data to the server
-    res.writeHead(201); // Use 201 Created for successful POST
-    res.end(JSON.stringify({ message: "Data created successfully" }));
-  } else if (req.method === "DELETE" && req.url === "/data") {
-    // Delete data
-    // For testing error response, you can simply return a 500 status code
-    res.writeHead(500);
-    res.end(JSON.stringify({ message: "Internal Server Error" }));
-  } else if (req.method === "PUT" && req.url === "/data") {
-    // Modify something
-    // For testing error response, you can return a 400 Bad Request
-    res.writeHead(400);
-    res.end(JSON.stringify({ message: "Bad Request" }));
-  } else {
-    // Send a 404 response for non-existing resources
-    res.writeHead(404);
-    res.end(JSON.stringify({ message: "Resource not found" }));
-  }
+app.use("/docs", express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("Welcome to my REST API!");
 });
 
-server.listen(port, hostname, () => {
+//dummy routing example
+app.get("/kukkuu", (request, response) => {
+  const myResponse = { message: "moikka" };
+  response.status(200);
+  response.json(myResponse);
+});
+
+//example generic items api
+//get all items
+app.get("/api/items", getItems);
+// get items by id
+app.get("/api/items/:id/", getItemsById);
+//modify
+app.put("/api/items");
+//post
+app.post("/api/items");
+//Remove
+app.delete("/api/items");
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ src/items.js
@@ -0,0 +1,37 @@
+// mock items data
+const items = [
+  { id: 5, name: "porkkana" },
+  { id: 6, name: "omena" },
+  { id: 19, name: "appelsiini" },
+];
+
+const getItems = (req, res) => {
+  res.json(items);
+};
+
+const getItemsById = (req, res) => {
+  // if item with id exists send it, otherwise send 404
+  console.log("getItemsById", req.params);
+  const item = items.find((element) => element.id == req.params.id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404);
+    res.json({ message: "Item not found." });
+  }
+};
+
+const postItem = (req, res) => {
+  console.log("new item posted", req.body);
+  if (req.body.name) {
+    items.push({ id: 0, name: req.body.name });
+    //TODO: check akst weeks example of generating an id
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(400);
+  }
+};
+
+// TODO: add deleteItem(), putItem() and routing for those in index.js
+
+export { getItems, getItemsById, postItem };
